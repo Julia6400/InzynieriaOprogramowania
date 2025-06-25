6c0fbafdf2faec05d07cda9eefb705337480099f
Julia6400 <you@example.com>
2025-06-25 18:15:17 +0200

    updated uni test for patient class, after updating service and treatment

--- vetclinic/tests/unit_test/test_patient.py
+++ vetclinic/tests/unit_test/test_patient.py
@@ -1,40 +1,43 @@
-# test/unit_tests/patient_test.py
 import unittest
+from datetime import date
 from models.patient import Patient
 from models.owner import Owner
 from models.services.treatment import Treatment
+from models.services.service import Service
 
 class TestPatient(unittest.TestCase):
 
     def setUp(self):
         self.owner = Owner("Anna", "Kowalska", "anna@example.com")
         self.patient = Patient(1, "Reksio", "pies", "beagle", 4, self.owner)
+        self.service = Service(101, "Szczepienie", "profilaktyka", "pies", 50.0)
+        self.treatment = Treatment(date(2025, 6, 20), [self.service], "Szczepienie przeciwko wściekliźnie")
 
     def test_initialization(self):
         self.assertEqual(self.patient.name, "Reksio")
         self.assertEqual(self.patient.species, "pies")
         self.assertEqual(self.patient.breed, "beagle")
         self.assertEqual(self.patient.age, 4)
-        self.assertEqual(str(self.patient.owner), "Anna Kowalska")
+        self.assertEqual(str(self.patient.owner), "Anna Kowalska - tel: anna@example.com")
 
     def test_str_representation(self):
-        expected = "Reksio (pies, beagle), 4 lat – Właściciel: Anna Kowalska"
+        expected = "Reksio (pies, beagle), 4 lat – Właściciel: Anna Kowalska - tel: anna@example.com"
         self.assertEqual(str(self.patient), expected)
 
     def test_add_treatment(self):
-        treatment = Treatment("Szczepienie", 50)
-        self.patient.add_treatment(treatment)
+        self.patient.add_treatment(self.treatment)
         self.assertEqual(len(self.patient.treatments), 1)
-        self.assertEqual(self.patient.treatments[0].name, "Szczepienie")
+        self.assertEqual(self.patient.treatments[0].description, "Szczepienie przeciwko wściekliźnie")
+        self.assertIn("Szczepienie", str(self.patient.treatments[0]))
 
     def test_add_medical_history(self):
         self.patient.add_medical_history("2025-06-21: Szczepienie")
         self.assertIn("2025-06-21: Szczepienie", self.patient.medical_history)
 
     def test_get_summary(self):
-        treatment = Treatment("Szczepienie", 50)
-        self.patient.add_treatment(treatment)
+        self.patient.add_treatment(self.treatment)
         self.patient.add_medical_history("2025-06-21: Szczepienie")
         summary = self.patient.get_summary()
         self.assertIn("Szczepienie", summary)
         self.assertIn("2025-06-21", summary)
+
